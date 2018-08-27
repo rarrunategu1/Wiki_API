b64211833886bcacbb581c1b362ad3e325b0c5d7
Rose Parker <parker.rose@ymail.com>
2018-08-27 23:10:10 +0000

    Search returns results

--- wikiapi.js
+++ wikiapi.js
@@ -14,19 +14,50 @@ fetchResults(searchQuery); //this can display the search query on the console if
 
 function fetchResults(searchQuery) {  //we are using fetch because it's built into the browser.  You won't need additional libraries
    const endpoint = `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=${searchQuery}`;  
+ 
   fetch(endpoint)
   .then(response => response.json())
   .then(data => {
     const results = data.query.search; //search is an arry of objects nested in query
   displayResults(results);
   })
-
-  .catch(() => console.log('An error ocurred'));
-  }
+    .catch(() => console.log('An error ocurred'));
+  
+}
   //declare the displayResults function under fetchResults
-  function displayResults(results) {
-    console.log(results);
-  }
+  //display results on the page by looping through each object in the results array and append into .searchResults
+
+function displayResults(results) {
+  //store a reference to .searchResults
+  const searchResults = document.querySelector('.searchResults');
+  //remove child elements
+  searchResults.innerHTML = '';
+  //loop over results array
+  results.forEach(result => {
+    //result here represents each object in our array
+    const url = encodeURI(`https://en.wikipedia.org/wiki/${result.title}`); //encode url with encode URI method changes the spaces it returns in the url to %20 to convert it into a format that can be transmitted over the net.
+   
+   //appending each result to the .searchResults node using insertAdjacentHTmL - a DOM API
+   searchResults.insertAdjacentHTML('beforeend',
+   `<div class="resultItem"> 
+   <h3 class="resultItem-title">
+   <a href ="${url}" target="_blank" rel="noopener">${result.title})</a>
+   </h3>
+   <span class="resultItem-snippet">${result.snippet}</span><br>
+   <a href="${url}" class="resultItem-link" target="_blank" rel="noopener">${url}</a>
+  });</div>`
+  );
+  });
+}
+  
+  //insertAdjacentHTML has two arguements, one for the position where we want to append the information, and a string of the html we want to return to the page.
+  //beforeend specifies that the HTML in the second argument be appended after the last child of searchResults.
+
+
+  
+
+ 
+
   
 
 //Grab the search query
@@ -39,3 +70,5 @@ form.addEventListener('submit', handleSubmit);
 
 const a = 'Hello';
  console.log('${a} World');  This is a new trick that uses ES6 Template literals instead of concatenation.  This will print Hello World. */
+ 
+
